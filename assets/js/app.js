2889d9a654e4b0ead0981930cd2670cc79550f08
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-04-09 13:47:37 +0530

    Animations added

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Reset scroll reveal everytime element goes off screen
+ScrollReveal({ reset: true });    
+
+// elements to use scroll reveal
+ScrollReveal().reveal('.custom-card', {delay : 300});
+ScrollReveal().reveal('.card', {delay: 300});
+ScrollReveal().reveal('#landing', {reset: false, delay: 300});
