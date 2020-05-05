b103ea68c9326c2d503dbd70d8633f56365651e4
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-05-06 01:35:17 +0530

    CSS refactored

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,3 @@ ScrollReveal({ reset: false });
 
 // elements to use scroll reveal
 ScrollReveal().reveal('.reveal-row', {delay : 300});
-ScrollReveal().reveal('h4', {delay : 300});
-ScrollReveal().reveal('#landing', {delay: 300});
